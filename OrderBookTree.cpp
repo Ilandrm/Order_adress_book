0121d0a14d41832922895c23f874329208f6a2fd
ilan DARMON <ilan.darmon@etu.univ-grenoble-alpes.fr>
2026-01-27 19:10:32 +0100

    add insert method

--- OrderBookTree.cpp
+++ OrderBookTree.cpp
@@ -4,7 +4,7 @@ OrderBookTree::OrderBookTree()
 {
 }
 
-void OrderBoolTree::rotateLeft(Order*& node)
+void OrderBookTree::rotateLeft(Order*& node)
 {
     Order* node_second = node->right;
 
@@ -27,7 +27,7 @@ void OrderBoolTree::rotateLeft(Order*& node)
     node->parent = node_second;
 }
 
-void OrderBoolTree::rotateRight(Order*& node)
+void OrderBookTree::rotateRight(Order*& node)
 {
     Order* node_second = node->left;
 
@@ -48,4 +48,78 @@ void OrderBoolTree::rotateRight(Order*& node)
     }
     node_second->right = node;
     node->parent = node_second;
+}
+
+void OrderBookTree::insert(Order* newOrder)
+{
+    Order* parent_order = nullptr;
+    Order* current_order = root
+    while (current_order != nullptr)
+    {
+        parent_order = current_order;
+        if (newOrder->price < current_order->price)
+        {
+            current_order = current_order->left;
+        } else if (current_order->price < newOrder->price)
+        {
+            current_order = current_order->right;
+        } else {
+            current_order = current_order->right;
+        }
+    }
+    newOrder->parent = parent_order;
+    newOrder->left = nullptr;
+    newOrder->right = nullptr;
+    newOrder->is_red = true;
+    fixOrder(newOrder);
+    if (parent_order == nullptr) {
+        root = newOrder;
+    } else if (newOrder->price < parent_order->price) {
+        parent_order->left = newOrder;
+    } else if (newOrder->price > parent_order->price) {
+        parent_order->right = newOrder;
+    } else {
+        parent_order->right = newOrder;
+    }
+}
+void OrderBookTree::fixInsert(Order* order) {
+    while (order->parent != nullptr && order->parent->is_red) {
+        if (order->parent == order->parent->parent->left) {
+            Order* oncle = order->parent->parent->right;
+
+            if (oncle != nullptr && oncle->is_red) {
+                order->parent->is_red = false;
+                oncle->is_red = false;
+                order->parent->parent->is_red = true;
+                order = order->parent->parent;
+            } else {
+                if (order == order->parent->right) {
+                    order = order->parent;
+                    rotateLeft(order);
+                }
+                order->parent->is_red = false;
+                order->parent->parent->is_red = true;
+                rotateRight(order->parent->parent);
+            }
+        } else {
+            Order* oncle = order->parent->parent->left;
+
+            if (oncle != nullptr && oncle->is_red) {
+                order->parent->is_red = false;
+                oncle->is_red = false;
+                order->parent->parent->is_red = true;
+                order = order->parent->parent;
+            } else {
+                if (order == order->parent->left) {
+                    order = order->parent;
+                    rotateRight(order);
+                }
+                order->parent->is_red = false;
+                order->parent->parent->is_red = true;
+                rotateLeft(order->parent->parent);
+            }
+        }
+    }
+
+    root->is_red = false;
 }
